752fecbad0ea18dabe89ad48940481caf531f673
George <george.smith@worc.ox.ac.uk>
2026-06-08 10:08:45 +0100

    Added about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<nav>
+    <a href='/'>home</a>
+    <a href='/about'>about</a>
+</nav>
+
 <h1>Welcome to Energy Hub</h1>
 <p>This project intends to bring all information on UK energy to one place</p>
---
+++ src/routes/about/about.svelte
@@ -0,0 +1,7 @@
+<nav>
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+<h1>About</h1>
+<p>This is my about page.</p>
